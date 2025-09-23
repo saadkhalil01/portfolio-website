ebcc654152caef105fb2a88cae37c6ab655b5c41
saadkhalil01 <saadkhalil9999@gmail.com>
2025-09-23 14:01:30 +0500

    Configure for Netlify deployment: static export, unoptimized images

---
+++ dist/_next/static/VNfn2r1c9q7Qtd9yYHE5L/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/050e6b1acd45d123.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ dist/_next/static/chunks/050e6b1acd45d123.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/1e931d85de0bebf8.js",
+  "static/chunks/065d71ecf7ed7627.js",
+  "static/chunks/turbopack-0274b254f9e595e7.js"
+])
